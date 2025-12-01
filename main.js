dd779ada9fe19d4542d0731c5bc219fa823c53fc
HARINI <harini@HARINIs-MacBook-Air.local>
2025-12-01 22:48:36 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    // These are the strings that will be typed out
+    strings: ["Frontend Developer", "UI/UX Developer", "Web Developer"], 
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
